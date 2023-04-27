31744a8283877974fa31d0090dd126292218ade1
shiwenlong12 <1300160147@qq.com>
2023-04-27 10:22:03 +0800

    4.27

---
+++ target/debug/deps/signal_defs-ab7c1cfcc7b662dc.d
@@ -0,0 +1,7 @@
+/home/swl/os/crate_module/signal-defs/target/debug/deps/signal_defs-ab7c1cfcc7b662dc.rmeta: src/lib.rs
+
+/home/swl/os/crate_module/signal-defs/target/debug/deps/libsignal_defs-ab7c1cfcc7b662dc.rlib: src/lib.rs
+
+/home/swl/os/crate_module/signal-defs/target/debug/deps/signal_defs-ab7c1cfcc7b662dc.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/libsignal_defs.d
@@ -0,0 +1,1 @@
+/home/swl/os/crate_module/signal-defs/target/debug/libsignal_defs.rlib: /home/swl/os/crate_module/signal-defs/src/lib.rs
